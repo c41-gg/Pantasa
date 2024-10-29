367ce8f7ffdec338f92d70fb2ad97518951d8cd9
c41-gg <92499689+c41-gg@users.noreply.github.com>
2024-10-29 20:38:08 +0800

    Update Tokenizer.py

--- rules/MPoSM/Tokenizer.py
+++ rules/MPoSM/Tokenizer.py
@@ -30,7 +30,7 @@ pos_tokenizer.add_tokens(pos_tag_vocab)
 
 # Save tokenizer for later use
 logging.info("Saving tokenizer...")
-pos_tokenizer.save_pretrained("model/pos_tokenizer")
+pos_tokenizer.save_pretrained("/content/Pantasa/rules/model/pos_tokenizer")
 logging.info("Tokenizer saved successfully.")
 
 # Load pre-trained model
@@ -43,7 +43,7 @@ model.resize_token_embeddings(len(pos_tokenizer))
 
 # Save the resized model
 logging.info("Saving the resized model...")
-model.save_pretrained("model/pos_model")
+model.save_pretrained("/content/Pantasa/rules/model/pos_model")
 logging.info("Resized model saved successfully.")
 
 print(len(pos_tokenizer))
